40786df8923592b498ec20b7724b7e42e0aa15b7
Mahmoudamin11 <mahmoudamin123321@gmail.com>
2024-07-30 09:19:00 +0300

    securing apiKey

--- src/components/fetchData.js
+++ src/components/fetchData.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const apiKey = `2f1b8e48747508db4050f87a65c42f1d`
+const apiKey = import.meta.env.VITE_API_KEY;
 const baseURL = 'https://api.themoviedb.org/3';
 
 export const api = axios.create({
